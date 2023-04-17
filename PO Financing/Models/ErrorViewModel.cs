24f29d0c5556f9c89a2634718c81b4f5c05f5fba
Proficient Mkansi <de.prof.codes@gmail.com>
2023-04-18 01:46:37 +0200

    Add project files.

---
+++ PO Financing/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PO_Financing.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
