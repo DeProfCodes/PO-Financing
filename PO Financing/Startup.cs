90e81cc1562d046eb2fc941758fd03e538d35f0f
Proficient Mkansi <de.prof.codes@gmail.com>
2023-05-13 11:28:04 +0200

    123

--- PO Financing/Startup.cs
+++ PO Financing/Startup.cs
@@ -57,12 +57,14 @@ namespace PO_Financing
 
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                app.UseStatusCodePages();
+                //app.UseDeveloperExceptionPage();
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                //app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseStatusCodePages();
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
